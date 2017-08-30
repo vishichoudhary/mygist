41446152244e422eb35c66f321d41fd8cbc61da2
vishichoudhary <vishal129.vk@gmail.com>
2017-08-30 20:28:58 +0530

    Added auth_fetch

--- fetch_gist.py
+++ fetch_gist.py
@@ -2,10 +2,23 @@ import json
 import requests
 class fetch_gist:
     "Simple fetch command class"
-    def __init__(self,url):
+    def __init__(self,url,pas='',user_name=''):
         self.url=url
+        self.pas=pas
+        self.user_name=user_name
     def fetch(self):
         req=requests.get(self.url)
+        res_ans=req.json()
+        if req.status_code==200:
+            print("Total no of public gists are "+str(len(res_ans)))
+            for i in range(len(res_ans)):
+                l=list(res_ans[i]['files'].keys())
+                print(l[0])
+        else:
+            print(req.status_code)
+            print("unsuccessful")
+    def fetch_auth(self):
+        req=requests.get(self.url,auth=(self.user_name,self.pas))
         res_ans=req.json()
         if req.status_code==200:
             print("Total no of public gists are "+str(len(res_ans)))
--- mygist.py
+++ mygist.py
@@ -3,7 +3,7 @@ import getpass
 import json
 from fetch_gist import fetch_gist as fg
 user_name=input("Enter user name ")
+passwd=getpass.getpass("Enter your robust password ")
 url='https://api.github.com/users/'+user_name+'/gists?per_page=30000'
-ob=fg(url)
-ob.fetch()
-
+ob=fg(url,passwd,user_name)
+ob.fetch_auth()

5466eac666ca4d84d6031b3de9f9900e3bb3fb37
khvilaboa <khvilaboa@esei.uvigo.es>
2017-10-06 18:55:15 +0200

    List gist lists without authentication

--- scripts/gists.py
+++ scripts/gists.py
@@ -49,9 +49,17 @@ class GistHandler:
 		self.user = user
 		self.passwd = passwd
 
-	def list(self):
-		url='https://api.github.com/users/'+self.user+'/gists'
-		req=requests.get(url,auth=(self.user,self.passwd))
+	def list(self, anonymous = False):
+		
+		if anonymous:
+			if self.user is None:
+				raise Exception("An username is required")
+			url='https://api.github.com/users/%s/gists' % self.user
+			req=requests.get(url)
+		else:
+			url='https://api.github.com/users/%s/gists' % self.user
+			req=requests.get(url,auth=(self.user, self.passwd))
+
 		res_ans=req.json()
 
 		if req.status_code==200:

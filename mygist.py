316e56b73c9f99165be79bbe2692871ba33c9047
khvilaboa <khvilaboa@esei.uvigo.es>
2017-10-06 14:55:56 +0200

    Get detailed info of a certain gist (by id)

--- mygist.py
+++ mygist.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 
-import argparse, getpass, os
+import argparse, getpass, os, sys
 from builtins import input
-from scripts.gists import Gist
+from scripts.gists import GistHandler, Gist
 
 parser = argparse.ArgumentParser(description="Auxiliar script to handle migraines related data")
 parser.add_argument("-u", "--username", default = None, help="Username of the GitHub account")
 parser.add_argument("-p", "--password", default = None, help="Password of the GitHub account")
 parser.add_argument("-c", "--create", action="store_true", help="To create a new gist")
-parser.add_argument("-t", "--title", default=None, help="Title of the new gist")
+parser.add_argument("-d", "--description", default=None, help="Description of the new gist")
 parser.add_argument("-f", "--files", default=None, help="File names to use as content")
 parser.add_argument("-b", "--public", action="store_true", help="Specifies if the gist will be public")
-parser.add_argument("-d", "--delete", default = None, help="To delete an existing gist (by id)")
+parser.add_argument("-r", "--remove", default = None, help="To delete an existing gist (by id)")
 parser.add_argument("-l", "--list", action = "store_true", help="List the previosly created gists")
+parser.add_argument("-i", "--info", default = None, help="Get detailed info of a gist (by id)")
 
 args = parser.parse_args()
 
 # Examples:
-# Delete a gist: python mygist.py -u [USERNAME] -d [GIST_ID]
-# Create a gist: python mygist.py -t [TITLE] -f [FILE_NAME] -p
+# Remove a gist: python mygist.py -u [USERNAME] -r [GIST_ID]
+# Create a gist: python mygist.py -d [DESCRIPTION] -f [FILE_NAME] -p
 # List gists: python mygist.py -l
 # See the help: python mygist.py -h
 
@@ -26,40 +27,61 @@ args = parser.parse_args()
 
 if args.username is None:
 	args.username = input("Enter your github user name: ")
-
+	
 if args.password is None:
 	args.password = getpass.getpass("Enter your github password: ")
+	
+gists = GistHandler(args.username, args.password)
 
-gists = Gist(args.username, args.password)
-
-if args.delete is not None:
-	if gists.delete(args.delete):
-		print("Deleted succesfully")
+				
+if args.info is not None:
+	gist = gists.retrieve(args.info)
+	
+	if gist is None:
+		raise Exception("Invalid gist ID")
+	
+	print("\nId: %s" % gist.id)
+	print("Description: %s" % gist.description)
+	print("Public: %s" % ("Yes" if gist.public else "No"))
+	print("Owner: %s" % gist.owner)
+	print("Created_at: %s" % gist.created_at)
+	print("Updated_at: %s" % gist.updated_at)
+	
+	print("\nFiles:")
+	for position, gist_file in enumerate(gist.files):
+		print("\t(%d)" % (position + 1))
+		print("\tFilename: %s" % gist_file.filename)
+		print("\tLanguage: %s" % gist_file.language)
+		print("\tSize: %s bytes\n" % gist_file.size)
+	
+elif args.remove is not None:
+	if gists.delete(args.remove):
+		print("Deleted succesfully!")
 	else:
-		print("There was a problem deleting the gist")
-
+		raise Exception("Invalid gist ID")
+		
 elif args.list:
 	gists.list()  # It would be better to retrieve the gists data and make here the prints
-
+	
 else:  # Create as default action
 	if args.title is None:
 		args.title = input("Enter the description for the gist: ")
-
+		
 	if not args.public:
 		args.public = input("Make this gist public (y/n): ") in ("y", "Y")
-
+		
 	files = args.files.split(",") if args.files else ""
-	existing_files = list(filter(os.path.exists, files))
-
+	existing_files = filter(os.path.exists, files)
+	
 	if args.files is None or len(existing_files) == 0:
 		print("No existing files were specified")
 		alt_file = ""
-
+		
 		while len(existing_files) == 0:
 			alt_file = input("Enter the file names (separated with commas): ")
-			files = list(map(lambda s: s.strip(), alt_file.split(",")))
-			existing_files = list(filter(os.path.exists, files))
-
+			files = map(lambda s: s.strip(), alt_file.split(","))
+			existing_files = filter(os.path.exists, files)
+			
 			if len(existing_files) == 0:
 				print("No existing files were specified")
 			elif len(existing_files) != len(files):
@@ -69,15 +91,15 @@ else:  # Create as default action
 						print(f)
 				if input("Continue anyway? (y/n): ") not in ("y", "Y"):
 					existing_files = []
-
-
+						
+		 
 	file_pairs = []  # (name, content)
 	for file_path in existing_files:
 		with open(file_path, "r") as myfile:
 			file_pairs.append((os.path.basename(file_path), myfile.read()))
-
+				
 	gist_id = gists.create(args.title, file_pairs, args.public)
-
+	
 	if gist_id is not None:
 		print("Created succesfully. Gist ID: %s" % gist_id)
 	else:
--- scripts/gists.py
+++ scripts/gists.py
@@ -1,17 +1,46 @@
-import requests, json
+import requests, json, pdb
+#import global_vars as gv
+
+class GistFile:
+	def __init__(self, json_data):
+		self.filename = json_data["filename"]
+		self.content = json_data["content"]
+		self.language = json_data["language"]
+		self.size = json_data["size"]
+		self.type = json_data["type"]
+		self.truncated = json_data["truncated"]
 
 class Gist:
+	def __init__(self, json_data):
+		self.url = json_data["url"]
+		self.forks_url = json_data["forks_url"]
+		self.commits_url = json_data["commits_url"]
+		self.id = json_data["id"]
+		self.description = json_data["description"]
+		self.public = json_data["public"]
+		self.owner = json_data["owner"]["login"]
+		self.truncated = json_data["truncated"]
+		self.comments = json_data["comments"]
+		self.created_at = json_data["created_at"]
+		self.updated_at = json_data["updated_at"]
+		
+		self.files = []
+		
+		for file_name in json_data["files"]:
+			self.files.append(GistFile(json_data["files"][file_name]))
+
+class GistHandler:
 	"Class for gist creation"
 	def __init__(self, user = None, passwd = None):
 		self.user = user
 		self.passwd = passwd
 		self.gists_number = 0
-
+		
 	def list(self):
 		url='https://api.github.com/users/'+self.user+'/gists'
 		req=requests.get(url,auth=(self.user,self.passwd))
 		res_ans=req.json()
-
+		
 		if req.status_code==200:
 			self.gists_number = len(res_ans)
             #print("Total no of gists are "+str(len(res_ans)))
@@ -23,25 +52,29 @@ class Gist:
 				print("Gist id is " + res_ans[i]['id'])
 		else:
 			raise Exception("Login failed.")
-
+			
+	def retrieve(self, gist_id):
+		self.url='https://api.github.com/gists/' + gist_id
+		req=requests.get(self.url, auth = (self.user,self.passwd))
+		return Gist(json.loads(req.text)) if req.status_code == 200 else None
+		
 	def create(self, description, files, public = True):
-
+        
 		files_dic = {}
 
 		for file_name, file_cont in files:
 			files_dic[file_name] = {"content": file_cont}
-
+		
 		self.send_dict={"description":description,
 						"public": public,
 						"files": files_dic
 						}
-
+						
 		req=requests.post('https://api.github.com/gists',auth=(self.user,self.passwd),data=json.dumps(self.send_dict))
 		return None if req.status_code != 201 else json.loads(req.text)["id"]
-
+		
 	def delete(self, gist_id):
 		self.url='https://api.github.com/gists/' + gist_id
 		req=requests.delete(self.url, auth = (self.user,self.passwd))
-		#print(req.status_code)
 		return req.status_code == 204
-
+		
